1d63b0cff78fbebd6038b67e0517bfe8df6863b7
Maik R��der <maikroeder@gmail.com>
2011-07-09 14:12:19 +0000

    =pep8

--- raisinpyramid/login.py
+++ raisinpyramid/login.py
@@ -6,12 +6,13 @@ from pyramid.url import route_url
 
 from raisinpyramid.security import USERS
 
+
 def login(request):
     login_url = route_url('login', request)
     referrer = request.url
     if referrer == login_url:
         # never use the login form itself as came_from
-        referrer = '/' 
+        referrer = '/'
     came_from = request.params.get('came_from', referrer)
     message = ''
     login = ''
@@ -21,20 +22,20 @@ def login(request):
         password = request.params['password']
         if USERS.get(login) == password:
             headers = remember(request, login)
-            return HTTPFound(location = came_from,
-                             headers = headers)
+            return HTTPFound(location=came_from,
+                             headers=headers)
         message = 'Failed login'
 
     return dict(
-        message = message,
-        url = request.application_url + '/login',
-        came_from = came_from,
-        login = login,
-        password = password,
+        message=message,
+        url=request.application_url + '/login',
+        came_from=came_from,
+        login=login,
+        password=password,
         )
-    
+
+
 def logout(request):
     headers = forget(request)
-    return HTTPFound(location = route_url('p1_homepage', request),
-                     headers = headers)
-    
+    return HTTPFound(location=route_url('p1_homepage', request),
+                     headers=headers)
