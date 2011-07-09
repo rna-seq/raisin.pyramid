a323ba6b6285bd10a51277c93a224caf70858467
Maik R��der <maikroeder@gmail.com>
2011-07-09 14:15:47 +0000

    =pep8

--- raisinpyramid/views.py
+++ raisinpyramid/views.py
@@ -3,7 +3,7 @@ from webob.exc import HTTPNotFound
 
 from pyramid.security import Allow
 from pyramid.security import Everyone
-from pyramid.security import authenticated_userid 
+from pyramid.security import authenticated_userid
 from pyramid.static import static_view
 
 import pyramid.renderers
@@ -15,9 +15,10 @@ from raisinpyramid import security
 
 STATIC_VIEW_OF_ICO = static_view('raisin.page:templates/static/', cache_max_age=3600)
 
+
 def box_view(request):
     logged_in = authenticated_userid(request)
-    security.check_permission(request, logged_in)   
+    security.check_permission(request, logged_in)
     context = Box(request)
     context.__acl__ = [(Allow, Everyone, 'view homepage'),
                        (Allow, 'group:encode', 'view encode project'),
@@ -39,9 +40,10 @@ def box_view(request):
         response.unicode_body = box_renderer({'context': context, 'request': request}, request)
     return response
 
+
 def page_view(request):
     logged_in = authenticated_userid(request)
-    security.check_permission(request, logged_in)   
+    security.check_permission(request, logged_in)
     context = Page(request)
     return dict(
         context=context,
