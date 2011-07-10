ef3dfd1daea6ab4e670e94323cbe03edc21c26ea
Maik R��der <maikroeder@gmail.com>
2011-07-10 20:26:35 +0000

    =pylint, refactor file extension handling

--- raisinpyramid/views.py
+++ raisinpyramid/views.py
@@ -1,8 +1,9 @@
+"""Views for boxes and pages"""
+
+import os.path
 from webob import Response
 from webob.exc import HTTPNotFound
 
-from pyramid.security import Allow
-from pyramid.security import Everyone
 from pyramid.security import authenticated_userid
 from pyramid.static import static_view
 
@@ -13,38 +14,45 @@ from raisin.restyler.box import Box
 
 from raisinpyramid import security
 
-STATIC_VIEW_OF_ICO = static_view('raisin.page:templates/static/', cache_max_age=3600)
+STATIC_VIEW_OF_ICO = static_view('raisin.page:templates/static/',
+                                 cache_max_age=3600)
 
 
 def box_view(request):
+    """View for boxes"""
     logged_in = authenticated_userid(request)
     security.check_permission(request, logged_in)
     context = Box(request)
-    context.__acl__ = [(Allow, Everyone, 'view homepage'),
-                       (Allow, 'group:encode', 'view encode project'),
-                       (Allow, 'group:big', 'view any project'),
-                      ]
 
-    if request['PATH_INFO'].endswith('.ico'):
+    file_extension = os.path.splitext(request['PATH_INFO'])[1]
+    if file_extension == '.ico':
         request.subpath = request['PATH_INFO'].split('/')
         return STATIC_VIEW_OF_ICO(context, request)
-    elif request['PATH_INFO'].endswith('.csv') or request['PATH_INFO'].endswith('.json'):
+    elif file_extension == '.html':
+        template = 'raisin.page:templates/box.pt'
+        box_renderer = pyramid.renderers.get_renderer(template)
+        response = Response()
+        value = {'context': context, 
+                 'request': request}
+        response.unicode_body = box_renderer(value, request)
+    elif file_extension == '.csv':
         if context.body is None:
             return HTTPNotFound()
         else:
             response = Response()
             response.body = context.body
     else:
-        box_renderer = pyramid.renderers.get_renderer('raisin.page:templates/box.pt')
-        response = Response()
-        response.unicode_body = box_renderer({'context': context, 'request': request}, request)
+        raise AttributeError(file_extension)
+
     return response
 
 
 def page_view(request):
+    """View for pages"""
     logged_in = authenticated_userid(request)
     security.check_permission(request, logged_in)
     context = Page(request)
+
     return dict(
         context=context,
         logged_in=logged_in,
