5e13a872c76b8af2de52b0d2670b543d4d82b72c
Maik R��der <maikroeder@gmail.com>
2011-07-10 20:33:41 +0000

    =remove localhost hack, check homepage through reqeust.mateched_rout

--- raisinpyramid/security.py
+++ raisinpyramid/security.py
@@ -14,21 +14,19 @@ def groupfinder(userid, request):
 def check_permission(request, logged_in):
     project_name = request.matchdict.get('project_name', None)
     anonymous_projects = PROJECTS.get("anonymous", [])
-    if request.matchdict == {}:
+
+    if request.matched_route.name == 'p1_homepage':
         # homepage
         pass
     elif project_name in anonymous_projects:
         # Anonymous can access
         pass
-    elif logged_in == None and not request.url.startswith("http://localhost:7777/"):
+    elif logged_in == None:
         # Show the login page if the user is not logged in
         # For test purposes, no login is needed when accessing through localhost
         raise Forbidden
     else:
-        if request.url.startswith("http://localhost:7777/"):
-            # No access restrictions when accessing through localhost
-            print request.url
-        elif logged_in == 'admin':
+        if logged_in == 'admin':
             # Allow access to all projects to the admin user
             pass
         elif logged_in in USERS.keys():
